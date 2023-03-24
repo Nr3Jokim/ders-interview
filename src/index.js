7aa10c5ad8848241b73c50012584dd09757d5e3b
Jakub Konecny <konecj22@gmail.com>
2023-03-24 15:36:09 +0100

    Adding package.json and basic structure

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <React.Fragment>
+        <h1>React App</h1>
+        <p>React App</p>
+    </React.Fragment>,
+    document.getElementById('root')
+);
